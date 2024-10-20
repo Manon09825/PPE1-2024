0df3739aae8c6f4ee6b9020f2fae5bc7d6973cdc
Manon <44001721@parisnanterre.fr>
2024-10-20 18:37:40 +0200

    comptes boucle

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,14 +1,7 @@
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2016
-echo "Locations en 2016:"
-grep "Location" *.ann > resultat_grep.txt
-wc -l resultat_grep.txt
-
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2017
-echo "Locations en 2017:"
-grep "Location" *.ann > resultat_grep.txt
-wc -l resultat_grep.txt
-
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2018
-echo "Locations en 2018:"
-grep "Location" *.ann > resultat_grep.txt
-wc -l resultat_grep.txt
+for an in 2016 2017 2018;
+do
+    cd ~/Desktop/Fac/Master/PPE/exercice1/ann/$an
+    echo "Locations en $an:"
+    grep "Location" *.ann > resultat_grep.txt
+    wc -l resultat_grep.txt
+done
