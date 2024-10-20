661783f5765cc7e5a96e50ceaf18906aca06e63f
Manon <44001721@parisnanterre.fr>
2024-10-20 18:48:26 +0200

    changements comptes par type par an

--- Exercices/comptes_par_type_par_an.sh
+++ Exercices/comptes_par_type_par_an.sh
@@ -1,14 +1,7 @@
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2016
-echo $1 "en 2016:"
-grep $1 *.ann > compte_par_type.txt
-wc -l compte_par_type.txt
-
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2017
-echo $1 "en 2017:"
-grep $1 *.ann > compte_par_type.txt
-wc -l compte_par_type.txt
-
-cd ~/Desktop/Fac/Master/PPE/exercice1/ann/2018
-echo $1 "en 2018:"
-grep $1 *.ann > compte_par_type.txt
-wc -l compte_par_type.txt
+for an in 2016 2017 2018;
+do
+    cd ~/Desktop/Fac/Master/PPE/exercice1/ann/$an
+    echo $1 "en $an:"
+    grep $1 *.ann > compte_par_type.txt
+    wc -l compte_par_type.txt
+done
