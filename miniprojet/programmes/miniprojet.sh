25fb51e6efab4aa70f4cba83387c0fcd82858bd9
Manon <44001721@parisnanterre.fr>
2024-11-16 11:54:30 +0100

    miniprojet2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,14 +1,44 @@
 #!/usr/bin/env bash
 
+echo "<html><table>
+    <head>
+    <tr>
+        <th>numero_ligne</th>
+        <th>url</th>
+        <th>code_http</th>
+        <th>encodage</th>
+        <th>nombre_mots</th>
+    </tr>
+    </head>" > ../tableaux/tableau-fr.html
+
+
 if [ $# -ne 1 ]
 then
 	echo "le script prend exactement 1 argument"
 	exit
 fi
 
-numero=1
+fichier_urls=$1
+
+nb_lignes=1
+
 while read -r line
 do
-	echo $numero $line
-	let numero=$numero+1
-done < "$1" > ../tableaux/tableau-fr.tsv
+    http_code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
+    encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+" | cut -d "=" -f2)
+    nbmots=$(lynx -dump -nolist $line | wc -w)
+
+    echo "<body>
+        <tr>
+            <td>$nb_lignes</td>
+            <td>$line</td>
+            <td>$http_code</td>
+            <td>$encodage</td>
+            <td>$nbmots</td>
+        </tr>
+    </body>"
+
+    nb_lignes=$(expr $nb_lignes + 1)
+done < $fichier_urls >> ../tableaux/tableau-fr.html
+echo " </table>
+    </html>" >> ../tableaux/tableau-fr.html
